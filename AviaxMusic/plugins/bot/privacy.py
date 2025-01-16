fc4173baa59b54484f74eb7336a25cb6010633e5
DeadliestOne <uncountableaura@gmail.com>
2025-01-16 22:03:53 +0530

    Update privacy.py

--- AviaxMusic/plugins/bot/privacy.py
+++ AviaxMusic/plugins/bot/privacy.py
@@ -7,7 +7,7 @@ import config
 TEXT = f"""
 **ᴘʀɪᴠᴀᴄʏ ᴘᴏʟɪᴄʏ ғᴏʀ {app.mention} !! 
 
-ɪғ ʏᴏᴜʀ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴏʀ ᴄᴏɴᴄᴇʀɴs ғᴇᴇʟ ғʀᴇᴇ ᴛᴏ ʀᴇᴀᴄʜ ᴏᴜᴛ ᴛᴏ ᴏᴜʀ [sᴜᴘᴘᴏʀᴛ ᴛᴇᴀᴍ](t.me/BeAkatsuki)  ᴏʀ ᴍᴇ @evyto** !!"""
+ɪғ ʏᴏᴜʀ ʜᴀᴠᴇ ᴀɴʏ ǫᴜᴇsᴛɪᴏɴs ᴏʀ ᴄᴏɴᴄᴇʀɴs ғᴇᴇʟ ғʀᴇᴇ ᴛᴏ ʀᴇᴀᴄʜ ᴏᴜᴛ ᴛᴏ ᴏᴜʀ [sᴜᴘᴘᴏʀᴛ ᴛᴇᴀᴍ](t.me/BeAkatsuki)  ᴏʀ ᴍᴇ @evyto !!**"""
 
 @app.on_message(filters.command("privacy"))
 async def privacy(client, message: Message):
@@ -15,7 +15,7 @@ async def privacy(client, message: Message):
         [
             [
                 InlineKeyboardButton(
-                    "𝑝𝑟𝑖𝑣𝑎𝑐𝑦 𝑝𝑜𝑙𝑖𝑐𝑦", url=https://telegra.ph/Privacy-Policy-for-01-10
+                    "𝑝𝑟𝑖𝑣𝑎𝑐𝑦 𝑝𝑜𝑙𝑖𝑐𝑦", url="https://telegra.ph/Privacy-Policy-for-01-10"
                 )
             ]
         ]
